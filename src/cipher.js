3b976fcc0df3388cfdc8c9bfb50dc0870789ab20
Evellyn <xe.uyemura@gmail.com>
2020-04-01 15:32:03 -0300

    Cifra

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,32 @@
 const cipher = {
-  // ...
-};
+    encode: function (mensagem, deslocamento) {
+        let alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+        let cripto = []
+        let mensagemCriptografada = []
 
-export default cipher;
+
+        for (let m = 0; m < mensagem.length; m++) {
+            
+            for (let a = 0; a < alfabeto.length; a++) {
+                if(alfabeto[a].toUpperCase() == mensagem[m].toUpperCase()) {
+                    cripto.push(a)
+                }
+            }
+        }
+
+        for (let c = 0; c < cripto.length; c++) {
+            let valor = Number(cripto[c]) + Number(deslocamento)
+            mensagemCriptografada.push(alfabeto[valor])
+            }
+        
+            let resultadoMensagem = document.querySelector("#resultado-mensagem")
+            resultadoMensagem.textContent = mensagemCriptografada
+        }
+        
+        
+        
+        
+
+       
+    }
+export default cipher; //Exportando a função cipher
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import cipher from './cipher.js';
+import cipher from './cipher.js'; //Importando o cipher.
 
-console.log(cipher);
+//Cifrar
+let botaoCifrar = document.querySelector("#cifrar");
+
+botaoCifrar.addEventListener("click", function(){
+    let divCifra = document.querySelector(".cifra");
+    divCifra.classList.remove("hide");
+})
+
+let resultado = document.querySelector("#resultado");
+
+resultado.addEventListener("click", function(event){
+    event.preventDefault();
+    let form = document.querySelector("#form-cifra");
+    let mensagem = form.mensagem.value.replace(" ", "|").split("")
+    let deslocamento = form.deslocamento.value
+    cipher.encode(mensagem, deslocamento)
+})
+
+    
+
+
+
+
+
+
+
+
+
