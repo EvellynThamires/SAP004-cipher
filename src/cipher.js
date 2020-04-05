81b59f0479b1ec9df3928c6797d177b36cfdd8ed
Evellyn <xe.uyemura@gmail.com>
2020-04-05 16:33:29 -0300

    Cifrar, decifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,41 +1,23 @@
 const cipher = {
-    encode: function (mensagem, deslocamento) {
-        let alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-        let cripto = []
-        let mensagemCriptografada = []
-        console.log(alfabeto.length)
-        for (let m = 0; m < mensagem.length; m++) {
-            
-            for (let a = 0; a < alfabeto.length; a++) {
-                if(alfabeto[a].toUpperCase() == mensagem[m].toUpperCase()) {
-                    cripto.push(a)
-                }
-            }
+    encode: function(mensagem, deslocamento){
+        let cript = ""
+        for (let i = 0; i < mensagem.length; i++) {
+            let letra = mensagem[i].toUpperCase().charCodeAt(0)
+            let letraASC = (( letra - 65 + Number(deslocamento)) % 26) + 65
+            cript += String.fromCharCode(letraASC)
         }
-
-        for (let c = 0; c < cripto.length; c++) {
-            let valor = 0
-            valor = Number(cripto[c]) + Number(deslocamento)
-            if(valor < alfabeto.length){
-                mensagemCriptografada.push(alfabeto[valor+1])
-            }
-            else{
-                console.log(" entrei")
-                let l = true
-                while (l === true) {
-                    if(valor >= alfabeto.length) {
-                        valor = valor - alfabeto.length
-                    }
-                    else {
-                        l = false 
-                    }
-                }
-                console.log(valor)
-                mensagemCriptografada.push(alfabeto[valor-1])
-            }
-        }
-            let resultadoMensagem = document.querySelector("#resultado-mensagem")
-            resultadoMensagem.textContent = mensagemCriptografada
+        return cript;
     },
-}
+    decode: function(mensagem, deslocamento){
+        let decript = ""
+        for (let i = 0; i < mensagem.length; i++) {
+            let letra = mensagem[i].charCodeAt(0)
+            let letraASC = (( letra + 65 - Number(deslocamento)) % 26) + 65
+            decript += String.fromCharCode(letraASC)
+        }
+        return decript
+    }   
+
+
+}  
 export default cipher; //Exportando a função cipher
--- src/index.js
+++ src/index.js
@@ -1,24 +1,52 @@
 import cipher from './cipher.js'; //Importando o cipher.
 
-//Cifrar
+// //Cifrar
 let botaoCifrar = document.querySelector("#cifrar");
 
-botaoCifrar.addEventListener("click", function(){
+botaoCifrar.addEventListener("click", function () {
     let divCifra = document.querySelector(".cifra");
     divCifra.classList.remove("hide");
 })
 
-let resultado = document.querySelector("#resultado");
+let resultado = document.querySelector("#resultadoEncode");
 
-resultado.addEventListener("click", function(event){
+resultado.addEventListener("click", function (event) {
     event.preventDefault();
     let form = document.querySelector("#form-cifra");
-    let mensagem = form.mensagem.value.replace(" ", "|").split("")
-    let deslocamento = form.deslocamento.value
-    cipher.encode(mensagem, deslocamento)
+
+    let mensagem = form.mensagemEncode.value.split("")
+    let deslocamento = form.deslocamentoEncode.value
+
+    let msgCripto = cipher.encode(mensagem, deslocamento)
+
+    let resultadoMensagem = document.querySelector("#resultadoMensagemCifrada")
+    resultadoMensagem.textContent = msgCripto
 })
 
-    
+//Decifrar
+let botaoDecifrar = document.querySelector("#decifrar");
+
+botaoDecifrar.addEventListener("click", function () {
+    let divDecifra = document.querySelector(".decifra");
+    divDecifra.classList.remove("hide");
+})
+
+let resultadoDecifra = document.querySelector("#resultadoDecode");
+
+resultadoDecifra.addEventListener("click", function (event) {
+    event.preventDefault();
+    let form = document.querySelector("#form-decifra");
+
+    let mensagem = form.mensagemEncode.value.split("")
+    let deslocamento = form.deslocamentoDecode.value
+
+    let msgDecript = cipher.decode(mensagem, deslocamento)
+
+    let resultadoMensagem = document.querySelector("#resultadoMensagemDecode")
+    resultadoMensagem.textContent = msgDecript
+})
+
+
 
 
 
